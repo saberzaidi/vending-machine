6153bcea248451a98f8da4c411068b4310449c52
saber zaidi <sabir_xaidi@yahoo.com>
2020-10-18 04:43:09 +0200

    Add project files.

---
+++ vending machine/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace vendingmachine
+{
+    class vendingmachine
+    {
+        private int depositedAmount;
+
+        public vendingmachine()
+        {
+            depositedAmount = 0;
+        }
+    public void Depositcoin(int coinamount)
+    {
+
+        depositedAmount += coinamount;
+
+    }
+    public void Getdrink()
+    {
+        if (depositedAmount >= COST_OF_DRINK)
+        {
+            Console.WriteLine("Your change is {0} cents", depositedAmount - COST_OF_DRINK);
+                depositedAmount = 0;
+        }
+        else
+            Console.WriteLine("insert more coin");
+    }
+
+    public void GetRefund()
+    {
+        Console.WriteLine("You were refunded {0}", depositedAmount);
+            depositedAmount = 0;
+    }
+
+    }
+}
---
+++ vending machine/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace vending_machine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var vendingmachine = new vendingmachine();
+            vendingmachine.DepositCoin(100);
+            vendingmachine.Getdrink();
+            vendingmachine.GetRefund();
+            Console.ReadLine();
+
+        }
+    }
+}
